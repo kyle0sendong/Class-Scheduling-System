95ed40f5fe13426946bdf3a40439832ccc048f34
Kyle A. Destura <kyle.afundar@gmail.com>
2021-12-08 22:07:01 +0800

    update scheduler page

--- Test-Codes/includes/templates/department.html.php
+++ Test-Codes/includes/templates/department.html.php
@@ -41,51 +41,51 @@ $output = '
             <tr>
                 <td><b>Mathematics<b></td>
                 <td>'.$mathCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=Mathematics"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>
 
             <tr>
                 <td><b>Science<b></td>
                 <td>'.$sciCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=Science"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             
             <tr>
                 <td><b>English<b></td>
                 <td>'.$engCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=English"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>
 
             <tr>
                 <td><b>Filipino<b></td>
                 <td>'.$filCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=Filipino"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>
             <tr>
                 <td><b>MAPEH<b></td>
                 <td>'.$mapehCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=MAPEH"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>
             <tr>
                 <td><b>AP<b></td>
                 <td>'.$apCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=AP"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>   
             <tr>
                 <td><b>ESP<b></td>
                 <td>'.$espCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=ESP"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>
             <tr>
                 <td><b>TLE<b></td>
                 <td>'.$tleCount.'</td>
-                <td>johnnn</td>
+                <td>TBD</td>
                 <td> <link><a href="teacher.php?dept=TLE"><button type="button" class="btn btn-primary">Show Faculties</button></a></link> </td>
             </tr>        
                         
--- Test-Codes/includes/templates/scheduler.html.php
+++ Test-Codes/includes/templates/scheduler.html.php
@@ -4,59 +4,87 @@
 
 $output = '
 
-<div class="sched">
-    <div class="add-module">
-      <h2>Set Schedule</h2>
-      <h1>Teacher</h1>
-      <input type="text" placeholder="Ex...Anne Cruz">
-      <h1>Subject</h1>
-      <input type="text" placeholder="Ex..Mathematics">
-      <div class="half">
-        <label for="grade">Grade Level</label>    
+
+<div class="add-module">
+
+  <div style="margin:5%">
+    <h2>Set Schedule</h2>
+    <h1>Teacher</h1>
+    <input type="text" placeholder="Ex...Anne Cruz">
+    <hr>
+    <h1>Subject</h1>
+    <input type="text" placeholder="Ex..Mathematics">
+    <hr>
+    <div style="margin:5%">
+      <label for="grade">Grade Level</label>    
         <select id="grade">
-        <option label="Grade 7"></option>
-        <option label="Grade 8"></option>
-        <option label="Grade 9"></option>
-        <option label="Grade 10"></option>
-        </select>
-        <label for="section">Section</label>
-        <select id="section">
-        <option label="A"></option>
-        <option label="B"></option>
-        <option label="C"></option>
-        <option label="D"></option>
-        <option label="E"></option>
-        <option label="F"></option>
-        </select>
-        <h1>Start Time</h1>
-        <input type="time" value="09:00">
+        <option label="Grade 7">7</option>
+        <option label="Grade 8">8</option>
+        <option label="Grade 9">9</option>
+        <option label="Grade 10">10</option>
+      </select>
+    
+      <label for="section">Section</label>
+      <select id="section">
+        <option label="A">A</option>
+        <option label="B">B</option>
+        <option label="C">C</option>
+        <option label="D">D</option>
+        <option label="E">E</option>
+        <option label="F">F</option>
+      </select>
     </div>
-      <div class="half">
-        <h1>End Time</h1>
-        <input type="time" value="10:00">
-    </div>  
-      <h1>Room</h1>
-      <input type="text" placeholder="Ex...Room1">
-      <h1>Day</h1>
-    <ul class="day-picker">
-        <li>Mon</li><li class="day-selected">Tue</li><li>Wed</li><li>Thu</li><li>Fri</li>
-    </ul>
-    <h1>Color</h1>
-    <ul class="color-picker">
-        <li class="blue"></li>
-        <li class="red"></li>
-        <li class="yellow"></li>
-        <li class="purple"></li>
-        <li class="green"></li>
-        <li class="orange"></li>
-        <li class="gray"></li>
-        <li class="navy"></li>
-    </ul>
-      <form>
-        <input type="submit" value="Add to Timetable">
-      </form>
-    </div>
+    <hr>
+  </div>
+
+
+  <div style="margin:5%">
+
+    <h1>Start Time</h1>
+    <input type="time" value="09:00">
 </div>
+
+<div style="margin:5%">
+    <h1>End Time</h1>
+    <input type="time" value="10:00">
+    <hr>
+</div>  
+
+
+<div style="margin:5%; width:100%;">
+  <h1>Day</h1>
+  <ul class="day-picker">
+      <li class="day-selected">Mon</li>
+      <li>Tue</li>
+      <li>Wed</li>
+      <li>Thu</li>
+      <li>Fri</li>
+  </ul>
+  <hr>
+</div>
+
+
+
+<div style="margin:5%; width:100%;">
+<h1>Color</h1>
+  <ul class="color-picker">
+      <li class="blue color-selected"></li>
+      <li class="red"></li>
+      <li class="yellow"></li>
+      <li class="purple"></li>
+      <li class="green"></li>
+      <li class="orange"></li>
+      <li class="gray"></li>
+      <li class="navy"></li>
+  </ul>
+</div>
+
+  <form style="margin-top: -10%;">
+    <input type="submit" value="Add to Timetable">
+  </form>
+  
+</div>
+
 
 
 
@@ -78,6 +106,7 @@ $output = '
       <span class="session-presenter">Room2</span>
       <button>Edit</button><button>Delete</button>
     </div>
+
     <h2 class="time-slot" style="grid-row: time-0800;">8:00am</h2>
 
     <div class="session session-2 track-2" style="grid-column: track-4; grid-row: time-0800 / time-1000;">
