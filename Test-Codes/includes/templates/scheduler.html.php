3c4f25bd1a84a4b2b287a3b57e123b436d582236
Kyle A. Destura <kyle.afundar@gmail.com>
2021-12-10 14:29:30 +0800

    update scheduler front+backend

--- Test-Codes/includes/templates/scheduler.html.php
+++ Test-Codes/includes/templates/scheduler.html.php
@@ -11,15 +11,14 @@ $output = '
 <div class="add-module">
 <h2>Set Schedule</h2>
 
-  <!--  -->
+<!-- Retrieving grade and section -->
   <div style="margin:5%">
-
   <div class="dropdown">
 ';
 
+/* For display which grade and section is selected */
   $grade;
   $section;
-
   if($isSetGradeSection) {
     $grade = $_GET['grade_section'];
     $section = substr($grade, -1);
@@ -27,56 +26,96 @@ $output = '
     $gradeSection = 'Grade ' . $grade . ' - ' . $section;
   }
   else
-    $gradeSection = 'Grade and Section';
-
+    $gradeSection = 'Select Grade and Section';
+  
 $output .= '
-    <button onclick="dropDown()" class="dropbtn">'.$gradeSection.'</button>
+    <button onclick="dropDown('.'\'grade_section\''.')" class="dropbtn">'.$gradeSection.'</button>
     <div id="grade_section" class="dropdown-content">
 ';
 
-    //retrieve grade and section for the form
+    //retrieve grade and section and show them as clickable links
     for($i = 7; $i <= 10; $i++) {
       $gradeLevel = retrieveAllId($pdo, 'grade_level', 'grade', $i);
       foreach($gradeLevel as $gradeSection) {
         $output .= '
           <a href="scheduler.php?grade_section='.$gradeSection['grade'].$gradeSection['section'].'">
-          Grade '.$gradeSection['grade'].$gradeSection['section'].' </a>
+          Grade '.$gradeSection['grade'].' - ' .$gradeSection['section'].' </a>
         ';
       }
     }
-    
+/* End of retrieving grade and section */
+
+
+/* For display which subject is selected if it is selected */
+$subjectSelected;
+if(isset($_GET['subject'])) 
+  $subjectSelected =  $_GET['subject'];
+else
+  $subjectSelected = 'Select Subject';
+
+/* For sending the current selected grade and section to the selected subject */
+  $gradeSection;
+  if($isSetGradeSection) 
+    $gradeSection =  $_GET['grade_section'];
+  else
+    $gradeSection = ' ';
+
+
 $output .= '
+  
+      </div> <!-- Grade section -->
     </div>
-  </div>
 
-      <hr>
-    
-  <form action="scheduler.php" method="post">
-    <label for="subject">Subjects</label>
-      <select name="subject" style="width:60%; margin-left: 5%;">
-        <option value="Mathematics">Mathematics</option>
-        <option value="Science">Science</option>
-        <option value="English">English</option>
-        <option value="Filipino">Filipino</option>
-        <option value="MAPEH">MAPEH</option>
-        <option value="AP">AP</option>
-        <option value="ESP">ESP</option>
-        <option value="TLE">TLE</option>
-      </select>
+    <hr> 
+    <label for="subject">Subjects</label> 
+    <div class="dropdown" style="width: 50%;">
+      <button onclick="dropDown('.'\'subject\''.')" class="dropbtn">'.$subjectSelected.'</button>
+      <div id="subject" class="dropdown-content">
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=Mathematics">Mathematics</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=Science">Science</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=English">English</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=Filipino">Filipino</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=MAPEH">MAPEH</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=AP">AP</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=ESP">ESP</a>
+        <a href="scheduler.php?grade_section='.$gradeSection.'&subject=TLE">TLE</a>
+      </div>
+    </div>
     <hr>
+';
 
+    /* Retrieve teacher by subject selected, data will be taken by what department they are in */
+    $allTeacher = retrieveAllId($pdo, 'teacher', 'dept', $subjectSelected);
+    $currentTeacher;  //if current teacher is already set
+    if(isset($_POST['teacher_id']))
+      $currentTeacher = $_POST['teacher_id'];
+    else
+      $currentTeacher = 'TBD';
+
+$output .= '
+    <form action="scheduler.php" method="post">
     <label for="teacher">Available Teachers</label> <br>
       <select name="subject" style="width:80%; margin: 2% 0 0 10%;">
-      <option>Mathematics</option>
+        <option>'.$currentTeacher.'</option>
+';
+      //display all the teacher as option
+        foreach($allTeacher as $teacher) {
+          $id = $teacher['id'];
+          $firstName = $teacher['firstName'];
+          $lastName = $teacher['lastName'];
+          $fullName = $firstName . ' ' . $lastName;
+          $output .= '<option value="'.$id.'">'.$fullName.'</option> ';
+        }
+
+$output .= '
       </select>
     <hr>
-
   </div>
 
 
   <div style="margin:5%">
   <h1>Time and Duration</h1>
-    <label for="time">Start Time</label>
+    <label for="start_time">Start Time</label>
 
       <select name="hour" style="width:28%; margin: 0 0 0 6%;">
           <option selected> 7 AM</option>
@@ -101,13 +140,13 @@ $output .= '
 
   <div style="margin:5%">
     <label for="duration">Duration</label>
-      <select name="subject" style="width:50%; margin: 0 0 0 10%;">
+      <select name="end_time" style="width:50%; margin: 0 0 0 10%;">
         <option>30 Min</option>
         <option selected>1 Hr</option>
         <option >1 Hr 30 Min</option>
         <option >2 Hr</option>
         <option >2 Hr 30 Min</option>
-        <option >3 Hr</option>
+        <option >3 Hrs</option>
       </select>
     <hr>
   </div>  
@@ -115,38 +154,37 @@ $output .= '
   <div style="margin:5%; width:100%;">
   <h1>Day</h1>
     <div class="day-picker"> 
-    
       <div>
         <label>
-          <input type="checkbox" name=""> 
+          <input type="checkbox" name="mon"> 
           <span style="background:#0075b6;">Mon</span>
           </label>
       </div>
 
       <div>
         <label>
-          <input type="checkbox" name=""> 
+          <input type="checkbox" name="tue"> 
           <span style="background: #008c9b;">Tue</span>
           </label>
       </div>
 
       <div>
         <label>
-          <input type="checkbox" name=""> 
+          <input type="checkbox" name="wed"> 
           <span style="background:#93388d;">Wed</span>
           </label>
       </div>
 
       <div>
         <label>
-        <input type="checkbox" name=""> 
+        <input type="checkbox" name="thu"> 
         <span style="background:#812438;">Thu</span>
         </label>
       </div>
 
       <div>
         <label>
-          <input type="checkbox" name=""> 
+          <input type="checkbox" name="fri"> 
           <span style="background:#80852e;">Fri</span>
           </label>
       </div>
