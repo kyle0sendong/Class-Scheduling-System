724bcae16b1c4d0a98866ecf115f897f6c083a7c
Kyle A. Destura <kyle.afundar@gmail.com>
2021-12-10 16:23:53 +0800

    fix timetable

--- Test-Codes/includes/templates/scheduler.html.php
+++ Test-Codes/includes/templates/scheduler.html.php
@@ -132,16 +132,13 @@ $output .= '
 
       <select name="min" style="width:22%; margin: 0 0 0 6%;">
         <option selected>00</option>
-        <option>15</option>
         <option >30</option>
-        <option >45</option>
       </select>
   </div>
 
   <div style="margin:5%">
     <label for="duration">Duration</label>
       <select name="end_time" style="width:50%; margin: 0 0 0 10%;">
-        <option>30 Min</option>
         <option selected>1 Hr</option>
         <option >1 Hr 30 Min</option>
         <option >2 Hr</option>
@@ -197,7 +194,7 @@ $output .= '
 </form>
 </div>
 
-
+<!-- TIME TABLE -->
 <div class="text">
   <h2 id="schedule-heading" style="margin-left: 15%; padding-top: 20px;"> Class Schedule</h2>
   <div class="schedule" aria-labelledby="schedule-heading">
@@ -209,52 +206,78 @@ $output .= '
     <span class="track-slot" aria-hidden="true" style="grid-column: track-5; grid-row: tracks;">Friday</span>
     
     <h2 class="time-slot" style="grid-row: time-0700;">7:00am</h2>
+    <h2 class="time-slot" style="grid-row: time-0730;"></h2>
     <h2 class="time-slot" style="grid-row: time-0800;">8:00am</h2>
+    <h2 class="time-slot" style="grid-row: time-0830;"></h2>
     <h2 class="time-slot" style="grid-row: time-0900;">9:00am</h2>
+    <h2 class="time-slot" style="grid-row: time-0930;"</h2>
     <h2 class="time-slot" style="grid-row: time-1000;">10:00am</h2>
+    <h2 class="time-slot" style="grid-row: time-1030;"></h2>
     <h2 class="time-slot" style="grid-row: time-1100;">11:00am</h2>
+    <h2 class="time-slot" style="grid-row: time-1130;"></h2>
     <h2 class="time-slot" style="grid-row: time-1200;">12:00pm</h2>
+    <h2 class="time-slot" style="grid-row: time-1230;"></h2>
     <h2 class="time-slot" style="grid-row: time-0100;">1:00pm</h2>
+    <h2 class="time-slot" style="grid-row: time-0130;"></h2>
     <h2 class="time-slot" style="grid-row: time-0200;">2:00pm</h2>
+    <h2 class="time-slot" style="grid-row: time-0230;"></h2>
     <h2 class="time-slot" style="grid-row: time-0300;">3:00pm</h2>
+    <h2 class="time-slot" style="grid-row: time-0330;"></h2>
     <h2 class="time-slot" style="grid-row: time-0400;">4:00pm</h2>
     
-    <div class="session track-1" style="grid-column: track-1; grid-row: time-0700 / time-0900;">
-      <span class="session-time">7:00 - 9:00</span>
-      <span class="session-track">Anne Cruz</span>
-      <span class="session-presenter">Room2</span>
-      <button>Delete</button>
+    <div class="session track-1" style="grid-column: track-1; grid-row: time-0700 / time-0800;">
+
+      <div class="session-time">
+        <div>7:00 - 11:30 MAPEH</div>
+        <div><button>X</button> </div>
+      </div>
+      <div class="session-track">
+        <div>Anneeeeee Cruzasdasd </div>
+      </div>
     </div>
 
-    <div class="session track-2" style="grid-column: track-2; grid-row: time-0700 / time-0900;">
-      <span class="session-time">7:00 - 9:00</span>
-      <span class="session-track">Anne Cruz</span>
-      <span class="session-presenter">Room2</span>
-      <button>Delete</button>
+    <div class="session track-2" style="grid-column: track-2; grid-row: time-0700 / time-0830;">
+      <div class="session-time">
+        <div>7:00 - 11:30 MAPEH</div>
+        <div><button>X</button> </div>
+      </div>
+      <div class="session-track">
+        <div>Anneeeeee Cruzasdasd </div>
+      </div>
     </div>
 
-    <div class="session track-3" style="grid-column: track-3; grid-row: time-0700 / time-0900;">
-      <span class="session-time">7:00 - 9:00</span>
-      <span class="session-track">Anne Cruz</span>
-      <span class="session-presenter">Room2</span>
-      <button>Delete</button>
+    <div class="session track-3" style="grid-column: track-3; grid-row: time-0700 / time-0830;">
+      <div class="session-time">
+        <div>7:00 - 11:30 MAPEH</div>
+        <div><button>X</button> </div>
+      </div>
+      <div class="session-track">
+        <div>Anneeeeee Cruzasdasd </div>
+      </div>
     </div>
 
-    <div class="session track-4" style="grid-column: track-4; grid-row: time-0700 / time-0900;">
-      <span class="session-time">7:00 - 9:00</span>
-      <span class="session-track">Anne Cruz</span>
-      <span class="session-presenter">Room2</span>
-      <button>Delete</button>
+    <div class="session track-4" style="grid-column: track-4; grid-row: time-0700 / time-0830;">
+      <div class="session-time">
+        <div>7:00 - 11:30 MAPEH</div>
+        <div><button>X</button> </div>
+      </div>
+      <div class="session-track">
+        <div>Anneeeeee Cruzasdasd </div>
+      </div>
     </div>
 
-    <div class="session track-5" style="grid-column: track-5; grid-row: time-0700 / time-0900;">
-      <span class="session-time">7:00 - 9:00</span>
-      <span class="session-track">Anne Cruz</span>
-      <span class="session-presenter">Room2</span>
-      <button>Delete</button>
+    <div class="session track-6" style="grid-column: track-5; grid-row: time-0700 / time-0830;">
+      <div class="session-time">
+        <div>7:00 - 11:30 MAPEH</div>
+        <div><button>X</button> </div>
+      </div>
+      <div class="session-track">
+        <div>Anneeeeee Cruzasdasd </div>
+      </div>
     </div>
 
-</div>
+    
+  </div>
 </div>
 
 
