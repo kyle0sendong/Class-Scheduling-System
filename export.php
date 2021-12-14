69b0dc3bb75452369600016dc71ad54c01f384ad
Gabjake <95706645+Gabjake@users.noreply.github.com>
2021-12-14 21:13:42 +0800

    still implemented thru sqli (updated with borders)

--- export.php
+++ export.php
@@ -1,44 +1,48 @@
 <?php  
- 
-//Database manipulation para malagyan ng whitespaces
 
-//Set up connection
-$conn = mysqli_connect('localhost', 'root', '', 'sched'); //Change database name
+//database connection
+$conn = mysqli_connect('localhost', 'root', '', 'sched'); 
 
-$filename = 'List of Records - ' .date('Y-m-d').'.csv';
-
-$sql = "SELECT * FROM try ORDER BY time"; //Change table name
-
-$result = mysqli_query($conn, $sql);
-
-$array = array();
-
-$file = fopen($filename, 'w');
-$array = array("Time","No. of Hours","Mon","Tue","Wed","Thu","Fri");
-fputcsv($file,$array);
-
-
-//Change variables
-while($row = mysqli_fetch_array($result)){
-
-	$time = $row['time'];
-	$hours = $row['hours'];
-	$day = $row['day'];
-	$section = $row['section'];
-
-	$array = array($time,$hours,$day,$section);
-	fputcsv($file,$array);
-}
-
-fclose($file);
-
-header("Content-Description: File Transfer");
-header("Content-Disposition: Attachment; filename=$filename");
-header("Content-Type: application/csv;");
-
-
-readfile($filename);
-unlink($filename);
-exit()
+//filename 
+header('Content-type: application/vnd-ms-excel');
+$filename = 'List of Records - ' .date('Y-m-d').'.xls';
+header("Content-Disposition:attachment;filename=\"$filename\"");
 ?>
 
+<!-- Html approach to make xls bordered -->
+<table class="table table-bordered">
+	<thead>
+		<tr>
+			<th style="border:2px solid black; text-align: center ">Time</th>
+			<th style="border:2px solid black; text-align: center">No. of Hours</th>
+			<th style="border:2px solid black; text-align: center">Monday</th>
+			<th style="border:2px solid black; text-align: center">Tuesday</th>
+			<th style="border:2px solid black; text-align: center">Wednesday</th>
+			<th style="border:2px solid black; text-align: center">Thursday</th>
+			<th style="border:2px solid black; text-align: center">Friday</th>
+		</tr>
+	</thead>
+	<tbody>
+	
+	<?php
+	// php approach to load all row elements
+	$query = "SELECT * FROM try"; 
+	$result = mysqli_query($conn, $query);
+	
+	while($row = mysqli_fetch_assoc($result)){
+	?>
+	<tr>
+			<!-- Printing each row element using php and html -->
+			<td style="border:2px solid black; text-align: center"><?php echo $row['time']; ?></td>
+			<td style="border:2px solid black; text-align: center"><?php echo $row['hours']; ?></td>
+			<td style="border:2px solid black; text-align: center"><?php echo $row['mon']; ?></td>
+			<td style="border:2px solid black; text-align: center"><?php echo $row['tue']; ?></td>
+			<td style="border:2px solid black; text-align: center"><?php echo $row['wed']; ?></td>
+			<td style="border:2px solid black; text-align: center"><?php echo $row['thu']; ?></td>
+			<td style="border:2px solid black; text-align: center"><?php echo $row['fri']; ?></td>
+		</tr>
+		<?php
+		}
+		?>
+	</tbody>
+</table>
