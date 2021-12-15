3f8a665eb8b3f4eb0b77a51b5f0142518a9b7481
Gabjake <95706645+Gabjake@users.noreply.github.com>
2021-12-15 22:49:48 +0800

    login php code

---
+++ logincode.php
@@ -0,0 +1,53 @@
+<?php 
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "schedule";
+
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+
+	die("failed to connect!");
+}
+
+session_start();
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from user where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: teacher.php");
+						die;
+					}
+				}
+			}
+			
+			echo "<font color=white> Wrong Username or Password!";
+		}else
+		{
+			
+		}
+	}
+
+?>
