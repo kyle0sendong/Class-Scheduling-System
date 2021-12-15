48fe2407c2488167eb92122042fc6bf8ed9b1aa6
Kyle A. Destura <kyle.afundar@gmail.com>
2021-12-15 23:14:43 +0800

    change link of css

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include 'logincode.php';
   <head>
     <title>Login Form</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="./includes/templates/css/login.css">
     <!-- Bootstrap CSS -->
     </head>
   <body>
